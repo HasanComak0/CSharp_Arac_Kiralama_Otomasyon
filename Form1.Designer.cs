9dd3e3fa691a4cd34bdc8580be0925deec8c59a4
Hasan Comak <hsncomak2222@gmail.com>
2025-11-30 22:27:18 +0300

    Paneller eklendi ve  Form İçeriği Denemesi yapmaya başlanacak

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Arac_Kiralama
 {
-    partial class Form1
+    partial class FRM_KullaniciOlusturGirisYap
     {
         /// <summary>
         ///Gerekli tasarımcı değişkeni.
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRM_KullaniciOlusturGirisYap));
+            this.pnl_KullaniciOlustur = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnl_GirisYap = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnl_KullaniciOlustur.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnl_GirisYap.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_KullaniciOlustur
+            // 
+            this.pnl_KullaniciOlustur.Controls.Add(this.label1);
+            this.pnl_KullaniciOlustur.Controls.Add(this.pictureBox1);
+            this.pnl_KullaniciOlustur.Location = new System.Drawing.Point(12, 12);
+            this.pnl_KullaniciOlustur.Name = "pnl_KullaniciOlustur";
+            this.pnl_KullaniciOlustur.Size = new System.Drawing.Size(374, 426);
+            this.pnl_KullaniciOlustur.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(104, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 139);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnl_GirisYap
+            // 
+            this.pnl_GirisYap.Controls.Add(this.label2);
+            this.pnl_GirisYap.Controls.Add(this.pictureBox2);
+            this.pnl_GirisYap.Location = new System.Drawing.Point(392, 12);
+            this.pnl_GirisYap.Name = "pnl_GirisYap";
+            this.pnl_GirisYap.Size = new System.Drawing.Size(374, 426);
+            this.pnl_GirisYap.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(126, 37);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(146, 139);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(87, 277);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kullanıcı Oluştur";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(100, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kullanıcı Oluştur";
+            // 
+            // FRM_KullaniciOlusturGirisYap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(780, 450);
+            this.Controls.Add(this.pnl_GirisYap);
+            this.Controls.Add(this.pnl_KullaniciOlustur);
+            this.Name = "FRM_KullaniciOlusturGirisYap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kullanıcı Oluştur | Giriş Yap";
+            this.pnl_KullaniciOlustur.ResumeLayout(false);
+            this.pnl_KullaniciOlustur.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnl_GirisYap.ResumeLayout(false);
+            this.pnl_GirisYap.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnl_KullaniciOlustur;
+        private System.Windows.Forms.Panel pnl_GirisYap;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Arac_Kiralama
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FRM_KullaniciOlusturGirisYap());
         }
     }
 }
